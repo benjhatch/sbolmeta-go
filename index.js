ef35cd535d9770f5356c435ce8e4d697167b271e
Chris Myers <myers@ece.utah.edu>
2016-12-26 00:57:53 -0700

    Add sequence ontology

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ module.exports = {
     summarizeDocument: require('./lib/summarizeDocument'),
     summarizeComponentDefinition: require('./lib/summarizeComponentDefinition'),
     summarizeSequence: require('./lib/summarizeSequence'),
-
+    sequenceOntology: require('./lib/sequence-ontology')
 }
 

42409d6c67f0e6961127eca3f35a58d8dca27ae0
Chris Myers <myers@ece.utah.edu>
2017-01-01 13:51:04 -0700

    Added summarize for modules

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
 
     summarizeDocument: require('./lib/summarizeDocument'),
     summarizeComponentDefinition: require('./lib/summarizeComponentDefinition'),
+    summarizeModuleDefinition: require('./lib/summarizeModuleDefinition'),
     summarizeSequence: require('./lib/summarizeSequence'),
     sequenceOntology: require('./lib/sequence-ontology')
 }
